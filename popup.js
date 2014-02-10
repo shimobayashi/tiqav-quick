021dbc5c89db90f9481cd90eab6da391cbd712f7
kimihito <tatsurotamashiro@gmail.com>
2014-02-10 20:01:03 +0900

    Markdownじゃなくてリンクを拾うように。選択するように

--- popup.js
+++ popup.js
@@ -3,21 +3,42 @@ $( function () {
     $('#query').keyup( function() {
         var val = $(this).val();
         if (val != '') {
-            var url = 'http://' + val + '.tiqav.com/';
-            $('#preview').attr('src', url);
-            $('#preview').show();
+            $('#preview-data').html('');
+            var json_url = 'http://api.tiqav.com/search.json?q='+val
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', json_url,true);
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    json = xhr.responseText;
+                    data = JSON.parse(json);
+                    $.each(data, function(k,v){
+                        url = 'http://tiqav.com/'+data[k].id+'.'+data[k].ext
+                        img = $('<img />').attr('src', url);
+                        $('#preview-data').append(img);
+                    });
+                }
+            }
+            xhr.send();
+            $('#preview-data').show();
         } else {
-            $('#preview').hide();
+            $('#preview-data').hide();
         }
     }).change();
+
+    $(document).on('click', 'img',function(){
+        $('img').removeClass('active');
+        $(this).addClass('active');
+        console.log($(this));
+        console.log('clicked');
+    });
 
     $('#query-form').submit( function() {
         var val = $('#query').val();
         if (val != '') {
-            var text = '![' + val + '](' + $('#preview').attr('src') + ')';
+            var text = $('.active').attr('src');
             chrome.extension.sendRequest({text: text}, function(response) {
                 console.log(response.text);
-                updateStatus('Markdown形式でコピーしました');
+                updateStatus('コピーしました');
             });
         } else {
             updateStatus('クエリーを入力してください');
